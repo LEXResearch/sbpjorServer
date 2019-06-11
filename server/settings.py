3231c15cbdb54e7b9753ba8ee216ade339a6a25d
Henrique <henriqmayer@gmail.com>
2019-06-11 19:18:40 -0300

    changed utc

--- server/settings.py
+++ server/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
 LANGUAGE_CODE = 'pt-br'
-TIME_ZONE = 'UTC-3'
+TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
