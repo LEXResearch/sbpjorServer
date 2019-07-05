82aeef3f30ea11241a08e280b5aaa5d51b04f3bd
Henrique <henriqmayer@gmail.com>
2019-07-05 15:30:02 -0300

    added create method to user serializer

--- social/serializers.py
+++ social/serializers.py
@@ -8,11 +8,18 @@ from django.contrib.auth import get_user_model
 UserModel = get_user_model()
 
 class SBPJorUserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = SBPJorUser
-        fields = ('imei', 'username', 'password', 'nome', 'telefone', 'email', 'data_nascimento', 'cpf', 'rg')
+        fields = ('username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = SBPJorUser(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
 
 class ContatoSerializer(serializers.ModelSerializer):
     class Meta:
